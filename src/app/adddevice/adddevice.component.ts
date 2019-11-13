37d26f5230dcf2b88840b26c1340ca70f3b58f0e
GuilhermeADF <fonseca.guilhermead@gmail.com>
2019-11-13 22:35:41 +0000

    Dialog Box

--- src/app/adddevice/adddevice.component.ts
+++ src/app/adddevice/adddevice.component.ts
@@ -3,21 +3,41 @@ import {DivisionService} from '../division.service';
 import {Division} from '../shared/models/division';
 import { Router, ActivatedRoute } from '@angular/router';
 import {Device} from '../shared/models/device';
+import {NgbModal, ModalDismissReasons} from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-adddevice',
   templateUrl: './adddevice.component.html',
-  styleUrls: ['./adddevice.component.css']
+  styleUrls: ['./adddevice.component.css'],
 })
 export class AdddeviceComponent implements OnInit {
 
   model = new Device ();
 
   divisionID: number;
+  closeResult: string;
   
-  constructor( private route : ActivatedRoute,private router: Router) {
+  constructor( private route : ActivatedRoute,private router: Router, private modalService: NgbModal) {
 
    }
+
+   open(content) {
+    this.modalService.open(content, {ariaLabelledBy: 'modal-basic-title'}).result.then((result) => {
+      this.closeResult = `Closed with: ${result}`;
+    }, (reason) => {
+      this.closeResult = `Dismissed ${this.getDismissReason(reason)}`;
+    });
+  }
+
+  private getDismissReason(reason: any): string {
+    if (reason === ModalDismissReasons.ESC) {
+      return 'by pressing ESC';
+    } else if (reason === ModalDismissReasons.BACKDROP_CLICK) {
+      return 'by clicking on a backdrop';
+    } else {
+      return  `with: ${reason}`;
+    }
+  }
 
   // getDivision(id:number):void{
    
