1ac64ffee13d0dcce331cfed7f1db39f09bd4528
Tekaichi <tekaichi.vi@gmail.com>
2019-11-13 22:36:57 +0000

    On going dev

--- src/app/shared/models/device.ts
+++ src/app/shared/models/device.ts
@@ -1,8 +1,22 @@
 export class Device {
     name:String;
-    url: String;
     position: {
         x: number,
         y: number
     }
+    device: SystemDevice
+}
+
+export class SystemDevice{
+
+    type: String;
+    image: ImageBitmap;
+    actions: Action[];
+    url: String;
+
+}
+
+export class Action{
+    command : String;
+
 }
