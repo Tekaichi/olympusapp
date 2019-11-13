1f4c09aaee5ae9d69c9319fbfb17a362be45a97f
Tekaichi <tekaichi.vi@gmail.com>
2019-11-13 22:37:13 +0000

    V2

--- src/app/shared/models/user.ts
+++ src/app/shared/models/user.ts
@@ -4,5 +4,4 @@ export class User {
     name : String;
     plant : Number;
     procedures :[];
-    availableDevices: []; //??? The system knows all of the available devices, BUT, should this be here?
 }
