1117209007f6d1425242c9bed61ce693c8f22b0c
António Ferraz <Akk0099@users.noreply.github.com>
2019-11-17 17:09:12 +0000

    Procedure run popup

--- src/app/homepage/homepage.component.ts
+++ src/app/homepage/homepage.component.ts
@@ -1,9 +1,10 @@
-import { Component, OnInit, ViewChild,ElementRef, AfterViewInit, ɵSWITCH_TEMPLATE_REF_FACTORY__POST_R3__ } from '@angular/core';
-import {DivisionService} from '../division.service';
-import {Division} from '../shared/models/division';
-import {ProcedureService} from '../procedures.service';
-import {Procedure} from '../shared/models/procedures';
+import { Component, OnInit, ViewChild, ElementRef, AfterViewInit, ɵSWITCH_TEMPLATE_REF_FACTORY__POST_R3__ } from '@angular/core';
+import { DivisionService } from '../division.service';
+import { Division } from '../shared/models/division';
+import { ProcedureService } from '../procedures.service';
+import { Procedure } from '../shared/models/procedures';
 import { Router, ActivatedRoute } from '@angular/router';
+import { NgbModal, ModalDismissReasons } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-homepage',
@@ -11,87 +12,117 @@ import { Router, ActivatedRoute } from '@angular/router';
   styleUrls: ['./homepage.component.css']
 })
 export class HomepageComponent implements OnInit {
-  
-  @ViewChild('layout',{static:true}) layout : ElementRef;
-  
 
-  divisions : Division[];
-  procedures : Procedure[];
+  @ViewChild('layout', { static: true }) layout: ElementRef;
+
+
+  divisions: Division[];
+  procedures: Procedure[];
   energy: String;
   time: String;
   temperature: String;
   humidity: String;
+  closeResult: string;
 
-  constructor(private divisionService : DivisionService,private procedureService : ProcedureService,  private router: Router) { 
-  
- 
+  constructor(private route: ActivatedRoute, private modalService: NgbModal, private divisionService: DivisionService, private procedureService: ProcedureService, private router: Router) {
+
+
     let date = new Date();
-    if(date.getHours() <10){
-      this.time  = "0"+ date.getHours().toString();
-    }else{
-      this.time  = date.getHours().toString();
+    if (date.getHours() < 10) {
+      this.time = "0" + date.getHours().toString();
+    } else {
+      this.time = date.getHours().toString();
     }
-    this.time +=":";
-    if(date.getMinutes() < 10){
-      this.time +="0";
+    this.time += ":";
+    if (date.getMinutes() < 10) {
+      this.time += "0";
     }
-    this.time +=  date.getMinutes().toString();
-    
+    this.time += date.getMinutes().toString();
+
+
+
+  }
+
+  open(content, procedure) {
+    this.modalService.open(content, { ariaLabelledBy: 'modal-basic-title' }).result.then((result) => {
+      this.closeResult = `Closed with: ${result}`;
+      if (result == "Confirm") {
+        console.log(procedure.id)
+        this.runProcedure(procedure.id);
+      }
+    }, (reason) => {
+      this.closeResult = `Dismissed ${this.getDismissReason(reason)}`;
+    });
+  }
+
+  //Placeholder para dar run do procedure
+  runProcedure(id: number) {
+
+  }
+
+  private getDismissReason(reason: any): string {
+    if (reason === ModalDismissReasons.ESC) {
+      return 'by pressing ESC';
+    } else if (reason === ModalDismissReasons.BACKDROP_CLICK) {
+      return 'by clicking on a backdrop';
+    } else {
+      return `with: ${reason}`;
+    }
   }
 
   //Isto deveria receber um id do user, se quisermos multiplos utilizadores numa sessão.
-  getDivisions():void{
+  getDivisions(): void {
     this.divisionService.getDivisions().subscribe(
       division => {
-      this.divisions = division;
+        this.divisions = division;
       }
     );
   }
   //Isto deveria receber um id do user, se quisermos multiplos utilizadores numa sessão.
-  getProcedures():void{
+  getProcedures(): void {
     this.procedureService.getTopProcedures().subscribe(
       procedure => {
-      this.procedures = procedure;
+        this.procedures = procedure;
       }
     );
   }
 
-  getInfo(){
-      //info
-      var info = [];
-      
-      //temperatura
-      var tempValue = 0;
-      var tempCount = 0;
+  getInfo() {
+    //info
+    var info = [];
 
-      //humidade
-      var humidityValue = 0;
-      var humidityCount = 0;
+    //temperatura
+    var tempValue = 0;
+    var tempCount = 0;
 
-      //energia
-      var energy = 0;
+    //humidade
+    var humidityValue = 0;
+    var humidityCount = 0;
 
-    this.divisions.forEach(function(division){
-      division.info.forEach(function(inf){
-        if(inf.description == "Temperatura"){
+    //energia
+    var energy = 0;
+
+    this.divisions.forEach(function (division) {
+      division.info.forEach(function (inf) {
+        if (inf.description == "Temperatura") {
           var splitted = inf.value.split("ºC", 2);
           tempValue += +splitted[0];
           tempCount++;
         }
-        if(inf.description == "Humidade"){
+        if (inf.description == "Humidade") {
           var splitted = inf.value.split("%", 2);
           humidityValue += +splitted[0];
           humidityCount++;
         }
-        if(inf.description == "Energy"){
+        if (inf.description == "Energy") {
           var splitted = inf.value.split("W", 2);
           energy += +splitted[0];
         }
       })
-     
+
     })
-    info.push(tempValue/tempCount)
-    info.push(humidityValue/humidityCount)
+    info.push(tempValue / tempCount)
+    info.push(humidityValue / humidityCount)
     info.push(energy)
     return info;
   }
@@ -104,57 +135,57 @@ export class HomepageComponent implements OnInit {
     this.energy = this.getInfo()[2] + 'W';
   }
 
-  goToSettings(): void{
+  goToSettings(): void {
     this.router.navigate(['/settings']);
   }
 
-  goToLogs(): void{
+  goToLogs(): void {
     this.router.navigate(['/logs']);
   }
 
-  goToAnalytics(): void{
+  goToAnalytics(): void {
     this.router.navigate(['/analytics']);
   }
 
-  goToProcedures(): void{
+  goToProcedures(): void {
     this.router.navigate(['/procedures']);
   }
 
-  goToDivision(id: number): void{
-    this.router.navigate(['/division',id]);
+  goToDivision(id: number): void {
+    this.router.navigate(['/division', id]);
 
   }
 
   ngAfterViewInit() {
- 
+
     let local = this;
-    this.divisions.forEach(function(division){
+    this.divisions.forEach(function (division) {
       local.drawDiv(division); //Draws the divisions. 
     })
-  
+
   }
 
-  drawDiv(division: Division){
-    
+  drawDiv(division: Division) {
+
     let from = division.layout.from;
     let to = division.layout.to;
-  
-    
-  
+
+
+
     let parentHeight = this.layout.nativeElement.offsetHeight;
-  
+
     //console.log(parentHeight);
-     
+
     //Fill the screen ? 
 
     let scale = 0.9;
-    let width = Math.abs(to.x -from.x)*scale;
-    let height = Math.abs(to.y -from.y)*scale;
-    
+    let width = Math.abs(to.x - from.x) * scale;
+    let height = Math.abs(to.y - from.y) * scale;
 
-    from.x *=scale;
-    from.y *=scale;
-    
+
+    from.x *= scale;
+    from.y *= scale;
+
 
     //Devices
     /*let vals = "";
@@ -169,19 +200,21 @@ export class HomepageComponent implements OnInit {
        //<app-type [input]...></app-type>
       vals +="<span style='position:absolute;top:"+pos.y+"vw; left:"+pos.x+"vw;'>"+ name+"</span>";
     })*/
-    
-    
-    
-    let style = "style='position:absolute;margin:0 auto;top:"+from.y+"vw;left:"+from.x+"vw;border:1px solid black;width:"+width+"vw; height:"+height+"vw;'" //+size+"'";//position + size;
+
+
+
+    let style = "style='position:absolute;margin:0 auto;top:" + from.y + "vw;left:" + from.x + "vw;border:1px solid black;width:" + width + "vw; height:" + height + "vw;'" //+size+"'";//position + size;
     //Insert division
-    this.layout.nativeElement.insertAdjacentHTML('beforeend', " <a href='division/"+division.id+"'> <div class='division'"+style+">"+
-    "<div  style='position:relative;top: 50%;left: 50%;transform: translate(-50%, -50%);'>"+division.title+"</div>"
-    +"</div> </a>  " );
-    
-    
-    
-    
-    
+    this.layout.nativeElement.insertAdjacentHTML('beforeend', " <a href='division/" + division.id + "'> <div class='division'" + style + ">" +
+      "<div  style='position:relative;top: 50%;left: 50%;transform: translate(-50%, -50%);'>" + division.title + "</div>"
+      + "</div> </a>  ");
+
+
+
+
+
   }
+
+
 
 }
