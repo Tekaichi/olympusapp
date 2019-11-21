fb5a5874cab359e1a643c94148da1b466801dd5e
JSantosBruv <jmfd.santos@campus.fct.unl.pt>
2019-11-21 01:06:32 +0000

    Landing page changes

--- src/app/main/main.component.ts
+++ src/app/main/main.component.ts
@@ -1,9 +1,16 @@
 import { Component, OnInit } from '@angular/core';
 import { Router, ActivatedRoute } from '@angular/router';
+
+declare var jQuery: any;
+
+
+
+
 @Component({
   selector: 'app-main',
   templateUrl: './main.component.html',
-  styleUrls: ['./main.component.css']
+  styleUrls: ['./main.component.css',
+                '/animations.scss']
 })
 export class MainComponent implements OnInit {
   
@@ -12,6 +19,70 @@ export class MainComponent implements OnInit {
  
 
   ngOnInit() {
+
+    (function($) {
+      "use strict"; // Start of use strict
+    
+      // Smooth scrolling using jQuery easing
+      $('a.js-scroll-trigger[href*="#"]:not([href="#"])').click(function() {
+        if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+          var target = $(this.hash);
+          target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+          if (target.length) {
+            $('html, body').animate({
+              scrollTop: (target.offset().top - 71)
+            }, 1000, "easeInOutExpo");
+            return false;
+          }
+        }
+      });
+    
+      // Scroll to top button appear
+      $(document).scroll(function() {
+        var scrollDistance = $(this).scrollTop();
+        if (scrollDistance > 100) {
+          $('.scroll-to-top').fadeIn();
+        } else {
+          $('.scroll-to-top').fadeOut();
+        }
+      });
+    
+      // Closes responsive menu when a scroll trigger link is clicked
+      $('.js-scroll-trigger').click(function() {
+        $('.navbar-collapse').collapse('hide');
+      });
+    
+      // Activate scrollspy to add active class to navbar items on scroll
+      $('body').scrollspy({
+        target: '#mainNav',
+        offset: 80
+      });
+    
+      // Collapse Navbar
+      var navbarCollapse = function() {
+        if ($("#mainNav").offset().top > 100) {
+          $("#mainNav").addClass("navbar-shrink");
+        } else {
+          $("#mainNav").removeClass("navbar-shrink");
+        }
+      };
+      // Collapse now if page is not at top
+      navbarCollapse();
+      // Collapse the navbar when page is scrolled
+      $(window).scroll(navbarCollapse);
+    
+      // Floating label headings for the contact form
+      $(function() {
+        $("body").on("input propertychange", ".floating-label-form-group", function(e) {
+          $(this).toggleClass("floating-label-form-group-with-value", !!$(e.target).val());
+        }).on("focus", ".floating-label-form-group", function() {
+          $(this).addClass("floating-label-form-group-with-focus");
+        }).on("blur", ".floating-label-form-group", function() {
+          $(this).removeClass("floating-label-form-group-with-focus");
+        });
+      });
+    
+    })(jQuery); // End of use strict
   }
 
   goToLogIn(): void {
