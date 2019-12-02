d2b0fca752d7f4c7a9149e0b434cbf81edd5455a
António Ferraz <Akk0099@users.noreply.github.com>
2019-12-02 17:40:47 +0000

    Max alertas ao mesmo tempo 4

---
+++ src/app/goBackButton/goBackButton.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-goBack',
+  templateUrl: './goBackButton.component.html',
+  styleUrls: ['./goBackButton.component.css']
+})
+export class goBackButton implements OnInit {
+
+
+  constructor() { 
+  }
+
+  ngOnInit() {
+  }
+
+}
